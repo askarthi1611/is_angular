aaa26caf880e2b4848567184c413e72ceb861726
KARTHIKEYAN <askarthi1611@gmail.com>
2023-11-21 21:45:08 +0530

    icon add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: '', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
