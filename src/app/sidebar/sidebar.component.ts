ad926a615d82176613787c1185d8cb387b04d31e
askarthi1611 <askarthi1611@gmail.com>
2023-11-22 12:35:25 +0530

    sidebar changes

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,5 +11,13 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+public navlink=[
+  {link:'/lib',title:'Package '},
+  {link:'/chips',title:' JS Chips '},
+  {link:'/table',title:' DataTable '},
+  {link:'/table_col',title:' DataTable Colapes '},
+  {link:'/asplit',title:'Angular split '},
+  {link:'/host-listener',title:'Angular Keyboard HostListener'},
+  {link:'/drag',title:'Draggable '},
+]
 }
